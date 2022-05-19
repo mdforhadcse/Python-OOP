13120b708d379f9fdadb99ea4f5e064af584b482
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-05-20 00:24:28 +0600

    Update class__init__method.py

--- class__init__method.py
+++ class__init__method.py
@@ -1,16 +1,35 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    def __init__(self, name: str, price: float, quantity=0):  # automatically execute this method
+        # print(f'Instance created: {name}')
+        # validation
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+
+        # Assigning self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+        # return x * y
 
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 15000
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = Item("Phone", 15000)
+# item1.name = "Phone" This job is done by "self.name = name"
+# item1.price = 15000
+# item1.quantity = 5
 
-item2 = Item()
-item2.name = "Laptop"
-item2.price = 40000
-item2.quantity = 3
-print(item2.calculate_total_price(item2.price, item2.quantity))
+item2 = Item("Laptop", 40000, 3)
+# item2.name = "Laptop" This job is done by "self.name = name"
+# item2.price = 40000
+# item2.quantity = 3
+
+print(item2.calculate_total_price())
+
+# print(item1.name)
+# print(item2.name)
+# print(item1.price)
+# print(item2.price)
+# print(item1.quantity)
+# print(item2.quantity)
